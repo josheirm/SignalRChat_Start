e66ca2550591ef2f1efe1b8a00d67e6f24be89af
josheir <josheir@outlook.com>
2019-12-18 18:50:20 -0500

    Multiple players works except when odd number of tabs opened and than the next player added after closed browser or close tab.

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-
+using System;
 
 
 
@@ -11,17 +11,30 @@ public class Clients
     public string Name { get; set; }
 }
 
+public class EachGame
+{
+    public Clients Playerone { get; set; }
+    public Clients Playertwo { get; set; }
+}
+
+
 
 
 namespace SignalRChat.Hubs
 {
-
+    
     public class ChatHub : Hub
     {
+        string PlayerOneConnId = null;
+        string PlayerTwoConnId = null;
+
+
         public static int whoseturn = 0;
         public static int integer = 0;
         public static readonly List<Clients> ClientList = new List<Clients>();
-
+        private static object _syncRoot = new object();
+        //private static readonly List<EachGame> games = new List<EachGame>();
+        //private static readonly Random random = new Random();
 
         //////////////
 
@@ -40,30 +53,95 @@ namespace SignalRChat.Hubs
 
         }
 
+
+        
+
         public async Task Register(string user, string message)
         {
-            Clients A_Client = new Clients();
-            A_Client.ConnectionId = Context.ConnectionId;
-            A_Client.Name = "a";
-            ClientList.Add(A_Client);
+           
+                Clients A_Client = new Clients();
+                A_Client.ConnectionId = Context.ConnectionId;
+                A_Client.Name = "a";
+                ClientList.Add(A_Client);
+           
 
 
-            integer++;
-            
+
+                //hides button
                 //await Clients.All.SendAsync("ReceiveMessage", user, message);
                 await Clients.Client(Context.ConnectionId).SendAsync("IsRegister",user, message);
-            if (integer == 2)
+
+
+            //EachGame Egame = new EachGame();
+           // Egame.Playerone.ConnectionId = "null";
+           // Egame.Playerone.Name = "a";
+            var breakout = 1;
+            
+            
+            
+            while (true)
             {
 
-                //await Clients.All.SendAsync("Printnames");
 
-                await Clients.Client(ClientList[0].ConnectionId).SendAsync("Printnames1");
-                await Clients.Client(ClientList[1].ConnectionId).SendAsync("Printnames2");
+                foreach (Clients element in ClientList)
+                {
+                    //not the current user
+                    if (element.ConnectionId != Context.ConnectionId)
+                    {
+                        //remove this in next for each loop
+                        PlayerOneConnId = Context.ConnectionId;
+                        //found a player
+                        //Egame.Playertwo.ConnectionId = element.ConnectionId;
+                        PlayerTwoConnId = element.ConnectionId;
 
+                        //adds to game list, probably not needed
+                        //games.Add(Egame);
+                        //remove other player
+                        ClientList.Remove(element);
+
+                        //find the current player
+                        foreach (Clients element2 in ClientList)
+                        {
+                            if (element2.ConnectionId == Context.ConnectionId)
+                            {
+                                //removes current player
+                                ClientList.Remove(element2);
+
+                                
+
+                                //propagates out of while loop
+                                breakout = 2;
+                               
+                            }
+                            break;
+                        }
+                      
+
+                    }
+
+                    if (breakout == 2)
+                    { break; }
+                }
+
+
+                if (breakout == 2)
+                { break; }
+            
             }
+
+            
+
+            
+            
+               
+                //assigns to connections to be named player one and player two
+                await Clients.Client(PlayerOneConnId).SendAsync("Printnames2");
+                await Clients.Client(PlayerTwoConnId).SendAsync("Printnames1");
+
+           
 
         }
 
-
+        
     }
 }
